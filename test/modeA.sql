18a4045cdfc8170b0eeafb145fe2fa35a23595e9
Codacy Archive <codacy-archive@local>
2026-01-05 12:18:07 +0000

    snapshot 2026-01-05T12:18:07.767308+00:00

--- test/modeA.sql
+++ test/modeA.sql
@@ -228,3 +228,66 @@ SELECT a AS 'abcd', b FROM t2 WHERE c=3;
 ...: The quick fox jumps over the lazy brown dog
   b: 2
 END
+
+# https://sqlite.org/forum/forumpost/2025-12-31T19:14:24z
+#
+# For legacy compatibility, ".header" settings are not changed
+# by ".mode" unless the --title or --reset option is used on .mode.
+#
+.testcase 600
+DROP TABLE IF EXISTS t1;
+CREATE TABLE t1(a,b,c);
+INSERT INTO t1 VALUES(1,2,3);
+.header on
+.mode csv
+SELECT * FROM t1;
+.check --glob a,b,c*
+
+.testcase 610
+.mode csv -reset
+SELECT * FROM t1;
+.check 1,2,3
+
+.testcase 620
+.mode tty
+.mode csv
+.header on
+SELECT * FROM t1;
+.check --glob a,b,c*
+
+.testcase 630
+.mode tty
+.mode csv --title on
+SELECT * FROM t1;
+.check --glob a,b,c*
+.testcase 631
+.mode tty
+.mode csv --title off
+SELECT * FROM t1;
+.check 1,2,3
+
+# Verification of claims about .insert mode in the climode.html
+# documentation.
+.testcase 700
+CREATE TABLE tbl1(one,two);
+INSERT INTO tbl1 VALUES('hello!',10),('goodbye',20);
+.mode insert new_table
+SELECT * FROM tbl1;
+.check <<END
+INSERT INTO new_table VALUES('hello!',10);
+INSERT INTO new_table VALUES('goodbye',20);
+END
+.testcase 710
+.mode insert new_table --titles on
+SELECT * FROM tbl1;
+.check <<END
+INSERT INTO new_table(one,two) VALUES('hello!',10);
+INSERT INTO new_table(one,two) VALUES('goodbye',20);
+END
+.testcase 720
+.mode insert new_table --titles off
+SELECT * FROM tbl1;
+.check <<END
+INSERT INTO new_table VALUES('hello!',10);
+INSERT INTO new_table VALUES('goodbye',20);
+END
